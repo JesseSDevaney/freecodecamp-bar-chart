28578652422a7f0613cfb0259ff9f88564d39467
Jesse Devaney <jessesdevaney@gmail.com>
2021-04-28 21:55:20 -0400

    Fetch project data

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let data;
+
+const getData = () => {
+  fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
+    .then(response => response.json())
+    .then(json => {
+      data = json;
+    })
+    .catch(error => {
+      console.log(error)
+    });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", getData);
+}
+else {
+  getData();
+}
