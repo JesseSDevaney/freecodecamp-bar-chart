0b25201b0188f8b49f80798f6a1b018e5767fe71
Jesse Devaney <jessesdevaney@gmail.com>
2021-04-29 20:40:53 -0400

    Configure prettier and eslint plugins

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 let data;
 
 const getData = () => {
-  fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
-    .then(response => response.json())
-    .then(json => {
+  fetch(
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+  )
+    .then((response) => response.json())
+    .then((json) => {
       data = json;
     })
-    .catch(error => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
-}
+};
 
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", getData);
-}
-else {
+} else {
   getData();
 }
